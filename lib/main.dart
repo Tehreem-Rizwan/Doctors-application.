00c68d05890b241cf664dba81ac7674287274eda
Tehreem Rizwan <its.tehreem2830@gmail.com>
2024-07-12 17:54:07 +0500

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:doctorsapp/consts/fonts.dart';
 import 'package:doctorsapp/views/login_view/login_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
